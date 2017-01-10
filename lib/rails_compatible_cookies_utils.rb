fe8e2054536b6be76b0b86f54e943e908c37f39c
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2017-01-10 13:19:50 -0200

    A more proper fix for Ruby 2.4
    by keeping it configurable rather than only working for the default
    cipher.

--- lib/rails_compatible_cookies_utils.rb
+++ lib/rails_compatible_cookies_utils.rb
@@ -61,7 +61,7 @@ class RailsCompatibleCookiesUtils
   end
 
   def encrypted_secret
-    @encrypted_secret ||= generate_key(@encrypted_salt)[0, 32]
+    @encrypted_secret ||= generate_key(@encrypted_salt)[0, OpenSSL::Cipher.new(@cipher).key_len]
   end
 
   def generate_key(salt)
